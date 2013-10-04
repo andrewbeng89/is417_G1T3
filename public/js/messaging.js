d6d2e949f4bbffdf8a856690908e58711d0810c9
andrewbeng89 <madbeng@gmail.com>
2013-10-04 16:22:53 +0800

    added test project

---
+++ public/js/messaging.js
@@ -0,0 +1,21 @@
+var webSocketsApp = angular.module('WebSocketsApp', []);
+
+webSocketsApp.controller('SocketsController', function($scope, $http) {
+
+    // Array representing the list of messages
+    $scope.messages = [];
+    
+    var socket = io.connect('/');
+    socket.on('message', function(data) {
+        console.log({
+        	incoming: data
+        });
+        $scope.messages.push(data);
+        $scope.$apply();
+        console.log($scope.messages)
+        socket.emit('reply', {
+            reply : 'hello'
+        });
+    });
+
+});
