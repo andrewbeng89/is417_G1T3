1714fc6fae5b9ba8a3e461c0fda4e120d80d8888
andrewbeng89 <madbeng@gmail.com>
2013-10-04 17:12:58 +0800

    upadted app

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ io.configure('development', function(){
 });
 
 io.sockets.on('connection', function (socket) {
-  for (var i = 0; i < 1000; i++) {
+  for (var i = 0; i < 100; i++) {
     socket.emit('message', { message: 'hello ' + (i+1) });
   };
   socket.on('reply', function (data) {
